ec7644205733aced0d30b3007d90655c432b338d
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-07-30 21:09:32 -0300

    -

--- src/main/java/com/gbrlbarreto/helpdesk/domain/Pessoa.java
+++ src/main/java/com/gbrlbarreto/helpdesk/domain/Pessoa.java
@@ -19,6 +19,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public abstract class Pessoa implements Serializable{
@@ -38,7 +39,7 @@ public abstract class Pessoa implements Serializable{
     protected String senha;
 
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "PERFIS")
+    @CollectionTable(name = "PERFIS", joinColumns = @JoinColumn(name = "pessoa_id"))
     protected Set<Integer> perfis = new HashSet<>();
 
     @JsonFormat(pattern = "dd/MM/yyyy")
